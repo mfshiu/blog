d97eec065305b120839f915c22c555c315b8a10a
mfshiu <avatar.xu@gmail.com>
2024-01-09 10:52:08 +0800

    layout

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ def home():
     return render_template('/index.html')
 
 
+@app.route('/art')
+def art():
+    return render_template('/art.html')
+
+
+@app.route('/photo')
+def photo():
+    return render_template('/photo.html')
+
+  
 
 if __name__ == '__main__':
     # app.run(debug=True, port=5050)
     # app.run(debug=True, host='0.0.0.0')
     app.run()
-     
