339df62b98a956572b7ffbb2cd40474b75f4bcd7
mfshiu <avatar.xu@gmail.com>
2024-01-18 19:12:00 +0800

    photo

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def home():
     return render_template('/index.html')
 
 
+@app.route('/archi')
+def archi():
+    return render_template('/archi.html')
+ 
+
 @app.route('/art')
 def art():
     return render_template('/art.html')
@@ -17,9 +22,15 @@ def art():
 def photo():
     return render_template('/photo.html')
 
-  
+
+@app.route('/photos/<string:id>')
+def photos(id):
+    return render_template(f'photos/photos-{id}.html')
+
+   
 
 if __name__ == '__main__':
     # app.run(debug=True, port=5050)
     # app.run(debug=True, host='0.0.0.0')
     app.run()
+      
