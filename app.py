66a0a6e4b7b1856794173d14af7298411339975f
mfshiu <avatar.xu@gmail.com>
2024-03-16 03:44:56 +0800

    ..

--- app.py
+++ app.py
@@ -1,31 +1,38 @@
-from flask import Flask, json, render_template
+from flask import Flask, json, render_template, request
+
   
 app = Flask(__name__)
 
-
+ 
 @app.route('/')
 def home():
-    return render_template('/index.html')
+    return render_template('/photo.html', current_path=request.path)
+    # return render_template('/index.html', current_path=request.path)
 
-
+  
 @app.route('/archi')
 def archi():
-    return render_template('/archi.html')
+    return render_template('/archi.html', current_path=request.path)
  
-
+ 
 @app.route('/art')
 def art():
-    return render_template('/art.html')
+    return render_template('/art.html', current_path=request.path)
 
 
 @app.route('/photo')
 def photo():
-    return render_template('/photo.html')
+    return render_template('/photo.html', current_path=request.path)
 
-
+ 
 @app.route('/photos/<string:id>')
 def photos(id):
     return render_template(f'photos/photos-{id}.html')
+
+
+@app.template_filter('is_active')
+def is_active(path, current_path):
+    return 'active' if path == current_path else ''
 
    
 
@@ -33,4 +40,4 @@ if __name__ == '__main__':
     # app.run(debug=True, port=5050)
     # app.run(debug=True, host='0.0.0.0')
     app.run()
-      
+ 
